8420894f074d3aef354fc08b7179427eeecd7157
Lisa Trevis <l_trevis@hotmail.com>
2021-02-21 11:17:49 -0800

    Fined NaN issue, added alert component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,70 @@
 import React, { useState, useEffect } from 'react'
+import Alert from './Alert'
 
 const App = () => {
   const [gallons, setGallons] = useState(0)
   const [dollars, setDollars] = useState(0)
-  const [price, setPrice] = useState(2.99)
+  const [price, setPrice] = useState(0)
   const [timeoutValue, setTimeoutValue] = useState()
   const [isPumping, setIsPumping] = useState(false)
-  const [activeKey, setActiveKey] = useState(0)
+  const [activeKey, setActiveKey] = useState('')
+  const [alert, setAlert] = useState({
+    msg: 'Please choose a gas type:',
+    type: 'placeholder',
+  })
+
+  const showAlert = (type = '', msg = '') => {
+    setAlert({ type, msg })
+  }
+
+  const clearPump = () => {
+    setIsPumping(false)
+    setGallons(0)
+    setDollars(0)
+    setPrice(0)
+    setActiveKey('')
+    setAlert({
+      msg: 'Please choose a gas type:',
+      type: 'placeholder',
+    })
+  }
+
+  const startPump = () => {
+    if (activeKey === '') {
+      showAlert('danger', 'Please choose a gas type!')
+    } else {
+      setIsPumping(true)
+    }
+  }
+
+  const stopPump = () => {
+    setIsPumping(false)
+  }
+
+  const handleClick = (e) => {
+    if (isPumping) {
+      setIsPumping(false)
+    }
+    setPrice(parseFloat(e.target.value))
+    setActiveKey(parseInt(e.target.getAttribute('data-key')))
+    setAlert({
+      msg: 'Gas type set!',
+      type: 'success',
+    })
+    setGallons(0)
+  }
 
   useEffect(() => {
-    if (dollars === isNaN) {
-      setDollars(0)
-    }
+    setPrice(price)
+    console.log(price)
+  }, [price])
+
+  useEffect(() => {
+    setActiveKey(activeKey)
+    console.log(activeKey)
+  }, [activeKey])
+
+  useEffect(() => {
     if (isPumping) {
       setTimeoutValue(setInterval(() => pumpGas(), 250))
     } else {
@@ -22,18 +75,7 @@ const App = () => {
 
   useEffect(() => {
     setDollars(price * gallons)
-  }, [gallons, price])
-
-  const handleClick = (e) => {
-    if (dollars === isNaN) {
-      setDollars(0)
-    }
-    setDollars(0)
-    setGallons(0)
-    setPrice(parseFloat(e.target.value))
-    setActiveKey(parseInt(e.target.getAttribute('data-key')))
-    setIsPumping(false)
-  }
+  }, [gallons])
 
   const pumpGas = () => {
     setGallons((prevGallons) => prevGallons + 0.1)
@@ -43,7 +85,7 @@ const App = () => {
     <main>
       <h1>Gas Pump</h1>
       <section>
-        <h3>Choose your gas type:</h3>
+        <Alert {...alert} />
         <div className='gas-prices'>
           <button
             data-key='1'
@@ -75,22 +117,19 @@ const App = () => {
         </div>
         <div className='startstop-btn'>
           {isPumping ? (
-            <button
-              type='button'
-              className='btn btn-stop'
-              onClick={() => setIsPumping(false)}
-            >
-              Stop
+            <button type='button' className='btn btn-stop' onClick={stopPump}>
+              Stop pump
             </button>
           ) : (
-            <button
-              type='button'
-              className='btn btn-start'
-              onClick={() => setIsPumping(true)}
-            >
-              Start
+            <button type='button' className='btn btn-start' onClick={startPump}>
+              Start pump
             </button>
           )}
+        </div>
+        <div className='clear'>
+          <button className='btn clear-btn' onClick={clearPump}>
+            Clear pump
+          </button>
         </div>
         <div className='meters'>
           <div className='gallon-counter'>
